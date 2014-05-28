9475da3feb66905ff7ac573032f487105fb9431e
hawkrobe <hawkrobe@indiana.edu>
2014-05-28 11:45:48 -0400

    Correctly returns bivariate normal mesh

--- R/plot_fit.R
+++ R/plot_fit.R
@@ -12,8 +12,8 @@ plot_fit <- function(fit_params) {
 bivar_norm <- function(fit_params, bin_width) {
   v <- array(0, dim = c(4,2,2));
   for (i in 1:4) {
-    cond = names(fit_params)[i];
-    v[i,,] <- matrix(data=c(1,fit_params$cond[3], fit_params$cond[3], 1), ncol = 2, nrow = 2);
+    cond = fit_params[[i]];
+    v[i,,] <- matrix(data=c(1, cond[3], cond[3], 1), ncol = 2, nrow = 2);
   }
   densfact = 3; # How many SDs to compute?
   xrange <- array(0, dim = c(4,2));
@@ -29,5 +29,22 @@ bivar_norm <- function(fit_params, bin_width) {
   y = seq(from = min(yrange), to = max(yrange), by = bin_width);
   lenx = length(x);
   leny = length(y);
-  return(xrange);
+  dist = array(0, dim = c(4, leny, lenx));
+  for (i in 1:4) {
+    rdcov = sqrt(det(v[i,,]));
+    incov = solve(v[i,,]);
+    cond = fit_params[[i]];
+    for (y_ind in seq(from=leny, to=1, by = -1)) {
+      for (x_ind in seq(from=1, to=lenx)) {
+        xy_val = c(x[x_ind], y[y_ind]);
+        val_mat = xy_val - c(cond[1], cond[2]); # Normalize
+        exp_term = t(val_mat) %*% incov %*% val_mat;
+        dist[i,y_ind,x_ind] = (1/(2*pi*rdcov)) * exp(-0.5 * exp_term);
+      }
+    }
+  }
+  return(list(dist = dist, 
+              x = x,
+              y = y,
+              v = v));
 }
