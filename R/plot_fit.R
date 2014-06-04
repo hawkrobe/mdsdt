31098fdafc6298561eed3b80cd563b5748f222fe
hawkrobe <hawkrobe@indiana.edu>
2014-06-04 14:06:32 -0400

    Added minimal documentation

--- R/plot_fit.R
+++ R/plot_fit.R
@@ -1,46 +1,59 @@
-plot_fit <- function(fit_params, xlab = 'x', ylab = 'y') {
-  bin_width = .25;
-  bounds = TRUE;
-  marginals = TRUE;
-  plot_fit = TRUE;
-  
+#' Plot the Gaussian contours and marginals of a given parameter fit.
+#'
+#' @param A parameter list, as returned by gaussian_fit() 
+#' @export
+plot_fit <- function(fit_params) {
+  bin_width=.25;
   bivar_obj = bivar_norm(fit_params, bin_width);
-  xlims = with(bivar_obj, c(x[1], x[length(x)]));
-  ylims = with(bivar_obj, c(y[1], y[length(y)]));
-  level = c(.12, .12); # Determines which contour to plot
-  minax = -2.5;
-  maxax = 4.5;
+  dist = bivar_obj$dist;
+  x = bivar_obj$x;
+  y = bivar_obj$y;
+  xlims = c(x[1], x[length(x)]);
+  ylims = c(y[1], y[length(y)]);
+  level = c(.1, .1); # Determines which contour to plot
+  
+  # Plot Gaussian contours 
   plot.new();
-  par(fig=c(0,1,0,1), pty='s', new=TRUE);
-  # Add axes
-  if (marginals || plot_fit) {
-    for (i in 1:4) {
-      cond = fit_params[[i]];
-      par(fig=c(0,1,0,1), pty='s', new=TRUE);
-      contour(x = bivar_obj$x, y = bivar_obj$y, z = bivar_obj$dist[i,,], 
-              levels = level, xlim = xlims, ylim = ylims,
-              drawlabels = FALSE, axes = FALSE,
-              xlab = xlab, ylab = ylab);
-      points(cond[1], cond[2], pch = '+');
-    }
-    abline(h=0);
-    abline(v=0);
-    margx = margy = list(aa=NULL,ab=NULL,ba=NULL, bb=NULL);
-    # Plot marginal on x dimension
-    for (i in 1:4) {
-      cond = fit_params[[i]];
-      xvar = bivar_obj$covars[i,1,1];
-      yvar = bivar_obj$covars[i,2,2];
-      margx[[i]] = (1 / sqrt(2*pi*xvar)) * exp(-.5*((bivar_obj$x-cond[1])/sqrt(xvar))^2);
-      margy[[i]] = (1 / sqrt(2*pi*yvar)) * exp(-.5*((bivar_obj$y-cond[2])/sqrt(yvar))^2);
-    }
-    plot(margx$aa);
-    plot(margx$ab, lty=2);
-    plot(margx$ba, lty=2);
-    plot(margx$bb);
+  par(fig=c(.3,1,.3,1), mar=c(2,.5,1,.5));
+  for (i in 1:4) {
+    cond = fit_params[[i]];
+    par(new = TRUE);
+    contour(x = x, y = y, z = dist[i,,], 
+            levels = level, xlim = xlims, ylim = ylims,
+            drawlabels = FALSE, axes = FALSE, asp=1);
+    points(cond[1], cond[2], pch = '+');
   }
+  
+  # Plot decision bounds
+  abline(h=0);
+  abline(v=0);
+
+  # compute marginals
+  margx = margy = list(aa=NULL,ab=NULL,ba=NULL, bb=NULL);
+  for (i in 1:4) {
+    cond = fit_params[[i]];
+    xvar = bivar_obj$covars[i,1,1];
+    yvar = bivar_obj$covars[i,2,2];
+    margx[[i]] = (1 / sqrt(2*pi*xvar)) * exp(-.5*((x-cond[1])/sqrt(xvar))^2);
+    margy[[i]] = (1 / sqrt(2*pi*yvar)) * exp(-.5*((y-cond[2])/sqrt(yvar))^2);
+  }
+  
+  # Plot X marginals
+  par(fig=c(.3,1,.1,.4), pty='m', xaxt = 'n', yaxt = 'n', new=TRUE);
+  plot(margx$aa,type='l');
+  lines(margx$ab,type='l',lty=2);
+  lines(margx$ba,type='l',lty=2);
+  lines(margx$bb,type='l');
+  
+  # Plot Y marginals
+  par(fig=c(.1,.3,.3,1), pty='m', xaxt = 'n', yaxt = 'n', new=TRUE);
+  plot(margy$aa,bivar_obj$y,type='l');
+  lines(margy$ab,bivar_obj$y, type='l',lty=2);
+  lines(margy$ba,bivar_obj$y,type='l',lty=2);
+  lines(margy$bb,bivar_obj$y,type='l');
 }
 
+# Returns the density of bivariate normal distribution over a grid of x,y values
 bivar_norm <- function(fit_params, bin_width) {
   # Compute covariance matrices
   covars <- array(0, dim = c(4,2,2));
